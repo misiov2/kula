a878b6756b4046b3e67be14511c824d907a1c292
misiov2 <bozydarkowalski692137420@gmail.com>
2024-08-23 08:53:05 +0200

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,19 @@
+function addBall() {
+    // Twoja funkcja do dodawania piłek
+}
+
+function toggleStats() {
+    // Twoja funkcja do przełączania statystyk
+}
+
+function updateBlur() {
+    // Twoja funkcja do aktualizacji rozmycia
+}
+
+function clearBalls() {
+    // Twoja funkcja do czyszczenia piłek
+}
+
+function changeBackground() {
+    // Twoja funkcja do zmiany tła
+}
