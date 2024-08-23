cf891a7d25187f021154783603c099a0e67da1f8
misiov2 <bozydarkowalski692137420@gmail.com>
2024-08-23 08:45:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Toggle menu visibility
     function toggleMenu() {
         let menuContainer = document.getElementById("menuContainer");
         menuContainer.classList.toggle("expanded");
@@ -12,50 +11,37 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    // Make menu draggable
-    dragElement(document.getElementById("menuContainer"));
-
-    function dragElement(elmnt) {
+    function makeMenuDraggable(elmnt) {
         let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        if (document.getElementById("menuToggle")) {
-            // if present, the header is where you move the DIV from:
-            document.getElementById("menuToggle").onmousedown = dragMouseDown;
-        } else {
-            // otherwise, move the DIV from anywhere inside the DIV:
-            elmnt.onmousedown = dragMouseDown;
-        }
+        document.getElementById("menuToggle").onmousedown = dragMouseDown;
 
         function dragMouseDown(e) {
             e = e || window.event;
             e.preventDefault();
-            // get the mouse cursor position at startup:
             pos3 = e.clientX;
             pos4 = e.clientY;
             document.onmouseup = closeDragElement;
-            // call a function whenever the cursor moves:
             document.onmousemove = elementDrag;
         }
 
         function elementDrag(e) {
             e = e || window.event;
             e.preventDefault();
-            // calculate the new cursor position:
             pos1 = pos3 - e.clientX;
             pos2 = pos4 - e.clientY;
             pos3 = e.clientX;
             pos4 = e.clientY;
-            // set the element's new position:
             elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
             elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
         }
 
         function closeDragElement() {
-            // stop moving when mouse button is released:
             document.onmouseup = null;
             document.onmousemove = null;
         }
     }
 
-    // Set up menu toggle
+    makeMenuDraggable(document.getElementById("menuContainer"));
+
     document.getElementById("menuToggle").addEventListener("click", toggleMenu);
 });
